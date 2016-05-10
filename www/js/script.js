4e7a87e43603df6e3c9c8f356e4c416803001ae9
PRAS Martin <martin.pras@epitech.eu>
2016-05-11 01:53:54 +0200

    lien moteur de recherche + include fichier jquery

--- www/js/script.js
+++ www/js/script.js
@@ -1,71 +1,66 @@
 'use strict';
 angular.module('filterExample', [])
   .controller('filterController', ['$scope', function($scope) {
-    
-
-
-
-
     $scope.fiches =
         [
   {
-    "index": 0,
+    "index": "savoirformaliserplan",
     "keywords": "stratégie plan stratégique forces faiblesses opportunités menaces débuter une période démarrer une période gérer son service établir une stratégie",
     "mainTitle": "Donner du sens",
     "title": "Savoir formaliser le plan stratégique",
   },
   {
-    "index": 1,
+    "index": "savoirformaliserambition",
     "keywords": "ambition mise en place opérationnel opérations après plan stratégique enjeu objectifs territoire actions mener",
     "mainTitle": "Donner du sens",
     "title": "Savoir formaliser l'ambition du territoire / service"
   },
 
   {
-    "index": 10,
+    "index": "savoirvaloriserindividua",
     "keywords": "motiver individuel collaborateur féliciter rendez-vous remobiliser encourager entretien récompenser résultat valoriser",
     "mainTitle": "Transmettre de l'énergie",
     "title": "Savoir valoriser individuellement"
   },
   {
-    "index": 11,
+    "index": "savoirmesurercollectif",
     "keywords": "faire un point réunion collective groupe finir terminer bilan équipe féliciter encourager mobiliser",
     "mainTitle": "Transmettre de l'énergie",
     "title": "Savoir mesurer le progrès en collectif"
   },
   {
-    "index": 12,
+    "index": "savoirfetersucces",
     "keywords": "féliciter groupe collectif encourager récompenser bilan faire un point résultats annoncer réunion succès",
     "mainTitle": "Transmettre de l'énergie",
     "title": "Savoir fêtez le succès"
   },
  
   {
-    "index": 20,
+    "index": "savoirfixerobjectif",
     "keywords": "entretien annonce responsabilité motiver objectifs définir smart pilotage",
     "mainTitle": "Développer l'exigence bienveillante",
     "title": "Savoir fixer les objectifs motivants"
   },
   {
-    "index": 21,
+    "index": "savoirformaliserlettre",
     "keywords": "mission début lettre commencement début nouveau motiver arrivée embauche prise de poste",
     "mainTitle": "Développer l'exigence bienveillante",
     "title": "Savoir formaliser une lettre de mission"
   },
   {
-    "index": 22,
+    "index": "savoirconcentrerenergie",
     "keywords": "priorité collectif groupe début motiver bilan nouvelle période",
     "mainTitle": "Développer l'exigence bienveillante",
     "title": "Savoir concentrer l'énergie des équipes sur le nombre limité des priorités"
   },
   {
-    "index": 23,
+    "index": "savoirdecliner",
     "keywords": "cadrer individuel actions clés accompagner entretien identifier prendre du recul prioriser",
     "mainTitle": "Développer l'exigence bienveillante",
     "title": "Savoir décliner des priorités en actions concrètes"
   },
   {
-    "index": 30,
+    "index": "savoirrealiser1",
     "keywords": "intégrer embauche adaptation démarrage prise de poste nouveau arrivée accueil accueillir arrivant",
     "mainTitle": "Proféssionaliser ses équipes",
     "title": "Savoir réaliser une intégration apprenante 1/2"
@@ -77,25 +72,25 @@ angular.module('filterExample', [])
     "title": "Savoir réaliser une intégration apprenante 2/2"
   },
   {
-    "index": 32,
+    "index": "savoirtraitererreur",
     "keywords": "Erreur problème trouver une solution remobiliser encourager apprendre faux pas entretien individuel réunion",
     "mainTitle": "Proféssionaliser ses équipes",
     "title": "Savoir développer les compétences / traiter une erreur pédagogique"
   },
   {
-    "index": 33,
+    "index": "savoirbriefercollaborateur",
     "keywords": "réunion individuel entretien briefer collaborateur motiver action mission préparer rdv",
     "mainTitle": "Proféssionaliser ses équipes",
     "title": "Savoir développer les compétences / briefer un collaborateur avant une action de jeu"
   },
   {
-    "index": 34,
+    "index": "savoirrealiserdebriefing",
     "keywords": "débrief faire un point collaborateur résultat tirer des enseignements valoriser fécilitier progresser entretien individuel",
     "mainTitle": "Proféssionaliser ses équipes",
     "title": "Savoir développer les compétences / réaliser un debriefing apprenant"
   },
   {
-    "index": 35,
+    "index": "savoiraccompagnerprojet",
     "keywords": "point mensuel bilan motiver progresser collaborateur développer entretien projet faire un point réunion",
     "mainTitle": "Proféssionaliser ses équipes",
     "title": "Savoir accompagner le projet professionnel"
@@ -107,44 +102,44 @@ angular.module('filterExample', [])
     "title": "Savoir accompagner la formation"
   },
   {
-    "index": 40,
+    "index": "savoirpiloter",
     "keywords": "inter service projet piloter transverse territoire collectif groupe impliquer motiver progresser",
     "mainTitle": "Manager la transversalité",
     "title": "Savoir piloter un projet transverse"
   },
   {
-    "index": 41,
+    "index": "savoiranimer",
     "keywords": "groupe animer transverse projet chantier motiver réunion collectif",
     "mainTitle": "Manager la transversalité",
     "title": "Savoir animer un groupe de travail transverse"
   },
   {
-    "index": 42,
+    "index": "savoigerer",
     "keywords": "projet collectif transverse motiver groupe autres services dynamiser intéractions",
     "mainTitle": "Manager la transversalité",
     "title": "Savoir gérer les intéractions avec d'autres services"
   },
 
   {
-    "index": 40,
+    "index": "savoirexpliquer",
     "keywords": "expliquer évoluer remotiver valeurs respecter enseigner progresser expliquer",
     "mainTitle": "Faire vivre les valeurs",
     "title": "Savoir expliquer les valeurs"
   },
   {
-    "index": 41,
+    "index": "savoirrappeler",
     "keywords": "expliquer faute remotiver écart règle progresser rappel entretien",
     "mainTitle": "Faire vivre les valeurs",
     "title": "Savoir rappeler une règle sans démotiver"
   },
   {
-    "index": 42,
+    "index": "savoirrecadrer",
     "keywords": "Recadrer faute dérive motiver rappel règle progresser hors jeu préparer",
     "mainTitle": "Faire vivre les valeurs",
     "title": "Savoir recadrer les dérives"
   },
   {
-    "index": 43,
+    "index": "savoirtraiterconflit",
     "keywords": "conflit collaborateurs éviter régler altercation problème interne différend désaccord",
     "mainTitle": "Faire vivre les valeurs",
     "title": "Savoir triater un conflit entre deux collaborateur"
