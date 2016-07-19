8cc04121fe17f5be4fb10617b4f5b11da84fdcb9
PRAS Martin <martin.pras@epitech.eu>
2016-07-19 12:59:40 +0200

    ignore

--- platforms/android/assets/www/js/favoris.js
+++ platforms/android/assets/www/js/favoris.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-    data = JSON.parse(localStorage.getItem("data"));
-    $("#bookmark").click(function(){
-    	$.each(data, function(index, value){
-			if ($(""))		    
-    	});
+	data = JSON.parse(localStorage.getItem("data"));
+	$(".bookmark").click(function(){
+		$.each(data, function(index, value){
+			id = $(".titrefiche").attr('id');
+			if (id == value.index)
+			{
+				if (value.fav == true)
+					value.fav = false;
+				else
+					value.fav = true;
+			}
+			data[index] = value;
+		});
+		localStorage.setItem("data", JSON.stringify(data));
+		console.log(JSON.parse(localStorage.getItem("data")));
     })
 });
