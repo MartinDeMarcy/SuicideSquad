e580dcd18c5549b2a63bf5f84d7340d8d9b61392
PRAS Martin <martin.pras@epitech.eu>
2016-07-27 03:31:53 +0200

    deplacement bootstrap et fini fav

--- www/js/favoris.js
+++ www/js/favoris.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 	data = JSON.parse(localStorage.getItem("data"));
+	$.each(data, function(index, value){
+			id = $(".titrefiche").attr('id');
+			if (id == value.index)
+			{
+				if (value.fav == true)
+				{
+					$(".fav").attr('src', '../../../img/fav.png');
+					$(".bookmark").css("background-color", "white");
+					$(".bookmark").css("border", "1px solid #D3D9E4")
+				}
+				else
+				{
+					$(".fav").attr('src', '../../../img/fav_vide.png');
+					$(".bookmark").css("background-color", "#59e09e");	
+					$(".bookmark").css("border", "0px")
+				}
+			}
+			data[index] = value;
+		});
 	$(".bookmark").click(function(){
 		$.each(data, function(index, value){
 			id = $(".titrefiche").attr('id');
@@ -7,12 +26,16 @@ $(document).ready(function(){
 			{
 				if (value.fav == true)
 				{
-					$(".fav").attr('src', '../../../img/fav_vide.png');			
+					$(".fav").attr('src', '../../../img/fav_vide.png');
+					$(".bookmark").css("background-color", "#59e09e");	
+					$(".bookmark").css("border", "0px")		
 					value.fav = false;
 				}
 				else
 				{
-					$(".fav").attr('src', '../../../img/fav.png');	
+					$(".fav").attr('src', '../../../img/fav.png');
+					$(".bookmark").css("background-color", "white");
+					$(".bookmark").css("border", "1px solid #D3D9E4")	
 					value.fav = true;
 				}
 			}
